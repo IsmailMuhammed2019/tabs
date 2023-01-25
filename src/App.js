cc6e3d5aa6031d1a1554eac199956ac1ccf8d889
ismailmuhammed2019 <ismailmuhammed2019@gmail.com>
2023-01-25 13:15:44 +0100

    set button functionality

--- src/App.js
+++ src/App.js
@@ -33,11 +33,38 @@ function App() {
       </section>
     )
   }
-
+const {id, title, dates, duties, company} = jobs[value]
 
  return (
-    <h4>Old Notes</h4>
-  )
+   <section className='section'>
+     <div className='title'>
+       <h2>Experience</h2>
+       <div className='underline'></div>
+     </div>
+     <div className='jobs-center'>
+       <div className='btn-container'>
+        {jobs.map((item, index) =>{
+          return (
+            <button key={index} onClick={() => setValue(index)} className={`job-btn ${index === value && 'active-btn'}`}>{item.company}</button>
+          )
+        })}
+       </div>
+       <article className='job-info' key={id}>
+         <h3>{title}</h3>
+         <h4>{company}</h4>
+         <p className='job-date'>{dates}</p>
+         {duties.map((duty, index) => {
+           return (
+             <div className='job-desc'>
+               <FaAngleDoubleRight className='job-icon' />
+               <p key={index}>{duty}</p>
+             </div>
+           )
+         })}
+       </article>
+     </div>
+   </section>
+ )
 }
 
 export default App
